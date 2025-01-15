f0510d0b118c85013f8f3d524957fcd998c525a7
J <kj5592305@gmail.com>
2025-01-15 15:07:28 +0900

    주석 설명

--- src/main/java/org/MenuItem.java
+++ src/main/java/org/MenuItem.java
@@ -4,39 +4,39 @@ public class MenuItem {
     private String name;
     private double price;
     private String description;
-
-    // Constructor
+    // 생성자 : 메뉴 항목의 이름, 가격, 설명을 초기화
     public MenuItem(String name, double price, String description) {
         this.name = name;
         this.price = price;
         this.description = description;
     }
 
-    // Getter and Setter methods
+    // Getter , Setter 메서드
+    // 메뉴 항목 이름을 반환함
     public String getName() {
         return name;
     }
-
+    // 메뉴 항목 이름을 설정함
     public void setName(String name) {
         this.name = name;
     }
-
+    // 메뉴 항목 가격을 반환함
     public double getPrice() {
         return price;
     }
-
+    // 메뉴 항목 가격을 설정함
     public void setPrice(double price) {
         this.price = price;
     }
-
+    // 메뉴 항목 설명을 반환함
     public String getDescription() {
         return description;
     }
-
+    // 메뉴 항목의 설명 설정함
     public void setDescription(String description) {
         this.description = description;
     }
-
+    // toString 메서드 : 메뉴 항목의 이름이나 가격, 설명을 문자열로 반환함
     @Override
     public String toString() {
         return name + " | W " + price + " | " + description;
